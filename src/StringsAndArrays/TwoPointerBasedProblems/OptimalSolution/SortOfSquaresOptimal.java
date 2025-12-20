28751f14efb8783ba9a56e902c3972f8e6f7ab0d
sree-gen-sree <amsreelakshmi20@gmail.com>
2025-12-20 14:36:07 +0530

    //https://leetcode.com/problems/is-subsequence/ //https://leetcode.com/problems/valid-palindrome/ //https://leetcode.com/problems/reverse-words-in-a-string-iii/ //https://leetcode.com/problems/reverse-string/

---
+++ src/StringsAndArrays/TwoPointerBasedProblems/OptimalSolution/SortOfSquaresOptimal.java
@@ -0,0 +1,31 @@
+package StringsAndArrays.TwoPointerBasedProblems.OptimalSolution;
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/
+import java.util.Arrays;
+
+public class SortOfSquaresOptimal {
+    public static void main(String[] args) {
+        int[] arr = {-8, -1, 0, 3, 5};
+        int[] sortedArray = FindSquares(arr);
+        System.out.println(Arrays.toString(sortedArray));
+    }
+
+    public static int[] FindSquares(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        int pos = arr.length-1;
+        int[] result = new int[arr.length];
+        while (left <= right) {
+            if (Math.abs(arr[left]) > Math.abs(arr[right])) {
+                result[pos] = arr[left] * arr[left];
+                left++;
+            }
+            else {
+                result[pos] = arr[right] * arr[right];
+                right--;
+            }
+            pos--;
+        }
+        return result;
+    }
+}
